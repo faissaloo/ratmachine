5d49f5d53c402d8509d967a1e62b961d6637fb26
faissaloo <faissaloo@gmail.com>
2020-01-10 20:04:26 +0000

    Fix formatting of tags next to each other

--- src/helpers/formatter_helper.cr
+++ src/helpers/formatter_helper.cr
@@ -7,20 +7,28 @@ module FormatterHelper
 		Regex.escape(escape(str))
 	end
 
+	def self.match_until
+		/(.*?)/
+	end
+
+	def self.isnt_escaped
+		/(?<!\\)/
+	end
+
 	def self.format(str : String)
 		escaped_str = self.escape(str)
-		escaped_str = escaped_str.gsub(/^(?<!\\)#{rxEsc(">")}(.*)$/m, "<span class=\"greentext\">#{escape(">")}\\1</span>")
-		escaped_str = escaped_str.gsub(/^(?<!\\)#{rxEsc("<")}(.*)$/m, "<span class=\"bluetext\">#{escape("<")}\\1</span>")
+		escaped_str = escaped_str.gsub(/^#{isnt_escaped}#{rxEsc(">")}#{match_until}$/m, "<span class=\"greentext\">#{escape(">")}\\1</span>")
+		escaped_str = escaped_str.gsub(/^#{isnt_escaped}#{rxEsc("<")}#{match_until}$/m, "<span class=\"bluetext\">#{escape("<")}\\1</span>")
 		escaped_str = escaped_str.gsub("\n", "<br/>")
-		escaped_str = escaped_str.gsub(/(?<!\\)#{rxEsc("`")}(.*)(?<!\\)#{rxEsc("`")}/, "<code>\\1</code>") #Bug: Things will be interpretted as formatting within the code tag
-		escaped_str = escaped_str.gsub(/(?<!\\)#{rxEsc("==")}(.*)(?<!\\)#{rxEsc("==")}/, "<span class=\"redtext\">\\1</span>")
-		escaped_str = escaped_str.gsub(/(?<!\\)#{rxEsc("%%")}(.*)(?<!\\)#{rxEsc("%%")}/, "<span class=\"spoiler\">\\1</span>")
-		escaped_str = escaped_str.gsub(/(?<!\\)#{rxEsc("$$")}(.*)(?<!\\)#{rxEsc("$$")}/, "<span class=\"shaketext\">\\1</span>")
-		escaped_str = escaped_str.gsub(/(?<!\\)#{rxEsc("$")}(.*)(?<!\\)#{rxEsc("$")}/, "<span class=\"rainbowtext\">\\1</span>")
-		escaped_str = escaped_str.gsub(/(?<!\\)#{rxEsc("#")}(.*)(?<!\\)#{rxEsc("#")}/, "<span class=\"threedtext\">\\1</span>")
-		escaped_str = escaped_str.gsub(/(?<!\\)#{rxEsc("!!")}(.*)(?<!\\)#{rxEsc("!!")}/, "<span class=\"glow\">\\1</span>")
-		escaped_str = escaped_str.gsub(/(?<!\\)#{rxEsc("**")}(.*)(?<!\\)#{rxEsc("**")}/, "<b>\\1</b>")
-		escaped_str = escaped_str.gsub(/(?<!\\)#{rxEsc("*")}(.*)(?<!\\)#{rxEsc("*")}/, "<i>\\1</i>")
+		escaped_str = escaped_str.gsub(/#{isnt_escaped}#{rxEsc("`")}#{match_until}#{isnt_escaped}#{rxEsc("`")}/, "<code>\\1</code>") #Bug: Things will be interpretted as formatting within the code tag
+		escaped_str = escaped_str.gsub(/#{isnt_escaped}#{rxEsc("==")}#{match_until}#{isnt_escaped}#{rxEsc("==")}/, "<span class=\"redtext\">\\1</span>")
+		escaped_str = escaped_str.gsub(/#{isnt_escaped}#{rxEsc("%%")}#{match_until}#{isnt_escaped}#{rxEsc("%%")}/, "<span class=\"spoiler\">\\1</span>")
+		escaped_str = escaped_str.gsub(/#{isnt_escaped}#{rxEsc("$$")}#{match_until}#{isnt_escaped}#{rxEsc("$$")}/, "<span class=\"shaketext\">\\1</span>")
+		escaped_str = escaped_str.gsub(/#{isnt_escaped}#{rxEsc("$")}#{match_until}#{isnt_escaped}#{rxEsc("$")}/, "<span class=\"rainbowtext\">\\1</span>")
+		escaped_str = escaped_str.gsub(/#{isnt_escaped}#{rxEsc("#")}#{match_until}#{isnt_escaped}#{rxEsc("#")}/, "<span class=\"threedtext\">\\1</span>")
+		escaped_str = escaped_str.gsub(/#{isnt_escaped}#{rxEsc("!!")}#{match_until}#{isnt_escaped}#{rxEsc("!!")}/, "<span class=\"glow\">\\1</span>")
+		escaped_str = escaped_str.gsub(/#{isnt_escaped}#{rxEsc("**")}#{match_until}#{isnt_escaped}#{rxEsc("**")}/, "<b>\\1</b>")
+		escaped_str = escaped_str.gsub(/#{isnt_escaped}#{rxEsc("*")}#{match_until}#{isnt_escaped}#{rxEsc("*")}/, "<i>\\1</i>")
 		escaped_str = escaped_str.gsub("\\","")
 	end
 
