fa5b937e2ce5710d48389b2d83a45394029fb63f
faissaloo <faissaloo@gmail.com>
2020-02-01 16:30:53 +0000

    Add user creation wizard and fix CAPTCHA issue in #3

--- src/ratmachine.cr
+++ src/ratmachine.cr
@@ -1,3 +1,24 @@
+require "file_utils"
 require "../config/*"
 
+if User.count == 0
+  puts "This seems to be your first time starting Ratmachine, you'll need to create an admin user for use on the /mod page"
+  username = nil
+  while username.nil? || username.blank?
+    printf "Username: "
+    username = gets
+  end
+  password = nil
+  while password.nil? || password.blank?
+    printf "Password: "
+    password = gets
+  end
+
+  unless username.nil? || password.nil?
+    User.create!(username: username.chomp, salted_hashed_password: Crypto::Bcrypt::Password.create(password.chomp).to_s)
+    puts "Creating CAPTCHA directory"
+    FileUtils.mkdir_p("public/dist/images/captcha")
+    puts "All done!"
+  end
+end
 Amber::Server.start
